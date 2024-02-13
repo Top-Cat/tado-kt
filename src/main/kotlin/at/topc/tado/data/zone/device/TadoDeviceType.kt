77199b0abc0691706fb33a4f0f8c437aa6e8283c
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-02-13 16:26:00 +0000

    Add more zone device types

--- src/main/kotlin/at/topc/tado/data/zone/device/TadoDeviceType.kt
+++ src/main/kotlin/at/topc/tado/data/zone/device/TadoDeviceType.kt
@@ -1,5 +1,20 @@
 package at.topc.tado.data.zone.device
 
 enum class TadoDeviceType {
-    RU01, VA01, VA02
+    RU02, // Wired thermostat v2
+    VA02, // Smart Radiator Thermostat v2
+    SU02, // Wireless temperature sensor
+    BR02, // Wireless receiver (Boiler)
+    BP02, // Wireless receiver (Programmer)
+    IB01, // Internet bridge
+    WR02, // Smart AC Control v2
+
+    /**
+     * Inactive products
+     */
+    RU01, // Wired thermostat v1
+    VA01, // Smart Radiator Thermostat v1
+    BU01, // Extension kit v1
+    BU02, // Extension kit v2
+    WR01, // Smart AC Control v1
 }
